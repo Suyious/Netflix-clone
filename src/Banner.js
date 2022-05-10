442101081843b0311d195ce6e247814447a18b32
Suyash <suyu162001@gmail.com>
2022-05-10 13:46:38 +0530

    Fine Tuning

--- src/Banner.js
+++ src/Banner.js
@@ -36,7 +36,7 @@ function Banner() {
       <div className="banner__content">
         <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
         <div className="banner__buttons">
-          <button className="banner__button">Play</button>
+          <button className="banner__button play">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h2 className="banner__description">{truncate(movie?.overview,150)}</h2>
--- src/Row.js
+++ src/Row.js
@@ -1,58 +1,32 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
-import Youtube from 'react-youtube';
-import movieTrailer from 'movie-trailer'
 
 function Row({ title, fetchURL, isBackdrop }) {
   const [movies, SetMovies] = useState([]);
-  const [trailerUrl,setTrailerUrl]=useState("");
   const baseURL = "https://image.tmdb.org/t/p/original/";
 
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchURL);
-      console.table(request.data.results);
+      // console.table(request.data.results);
       SetMovies(request.data.results);
       return request;
     }
     fetchData();
   }, [fetchURL]);
 
-  const opts = {
-    height: '390',
-    width: '640',
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-    },
-  };
-
-  const handleClick=(movie)=>{
-    if (trailerUrl){
-      setTrailerUrl('');
-    } else {
-      movieTrailer(movie?.name || "")
-      .then((url)=>{
-        const urlParams = new URLSearchParams(new URL(url).search);
-        setTrailerUrl(urlParams.get("v"));
-      })
-      .catch((error)=>console.log(error));
-    }
-  }
-
 
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
-        {movies.map((movie) => (
-          <div className={`row__movie ${isBackdrop && "row__movie_backdrop"}`}>
+        {movies.map((movie, i) => (
+          <div key={i} className={`row__movie ${isBackdrop && "row__movie_backdrop"}`}>
             <img
               key={movie.id}
               className="row__poster"
-              onClick={()=> handleClick(movie)}
               src={`${baseURL}${
                 isBackdrop ? movie.backdrop_path : movie.poster_path
               }`}
@@ -63,7 +37,6 @@ function Row({ title, fetchURL, isBackdrop }) {
           </div>
         ))}
       </div>
-      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
