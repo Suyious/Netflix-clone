993bf50a26a065d8df3c829c7806c6cc98a8111e
Suyash <suyu162001@gmail.com>
2022-05-10 14:49:38 +0530

    Minor Adjustments

--- src/Row.js
+++ src/Row.js
@@ -10,12 +10,24 @@ function Row({ title, fetchURL, isBackdrop }) {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchURL);
-      // console.table(request.data.results);
+      console.log(request.data.results);
       SetMovies(request.data.results);
       return request;
     }
     fetchData();
   }, [fetchURL]);
+
+  const Star = ({rating}) => {
+    if(rating <= 5){
+      return(
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M288 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.995 275.8 .0131 287.1-.0391L288 439.8zM433.2 512C432.1 512.1 431 512.1 429.9 512H433.2z"/></svg>
+      )
+    } else {
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z"/></svg>
+      )
+    }
+  }
 
 
   return (
@@ -33,7 +45,8 @@ function Row({ title, fetchURL, isBackdrop }) {
               alt={movie.title || movie.name}
               loading="lazy"
             />
-            <h1>{movie.title || movie.name}</h1>
+            <h1 className="row__movie_title">{movie.title || movie.name}</h1>
+            <h1 className="row__movie_rating"><Star rating={movie.vote_average}/>{movie.vote_average}</h1>
           </div>
         ))}
       </div>
